b79e9d8953d2106f3853d8d92aa5285c26ce5c83
Mathias Claesson <mc22ft@student.lnu.se>
2016-04-26 11:51:56 +0200

    Funktion login

--- src/ProfileMaker/Controllers/AuthController.cs
+++ src/ProfileMaker/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using ProfileMaker.Models;
+using ProfileMaker.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +11,47 @@ namespace ProfileMaker.Controllers
 {
     public class AuthController : Controller
     {
+        private SignInManager<ProfileUser> _signInManager;
+
+        public AuthController(SignInManager<ProfileUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult Login()
         {
             if (User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("ProfileUser", "App");
+            }
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel vm, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(vm.UserName,
+                                                                      vm.Password,
+                                                                      true, false);
+                if (signInResult.Succeeded)
+                {
+                    if (string.IsNullOrWhiteSpace(returnUrl)
+                    {
+                        return RedirectToAction("ProfileUser", "App");
+                    }
+                    else
+                    {
+                        return Redirect(returnUrl);
+                    }
+                   
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password incorrect");
+                }
             }
 
 
