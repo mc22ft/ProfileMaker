11a62aba081e4b882f379db7426daeaaa5b84908
Mathias Claesson <mc22ft@student.lnu.se>
2016-04-26 11:41:13 +0200

    login view

--- src/ProfileMaker/Controllers/AuthController.cs
+++ src/ProfileMaker/Controllers/AuthController.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNet.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProfileMaker.Controllers
 {
-    public class AuthController
+    public class AuthController : Controller
     {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("ProfileUser", "App");
+            }
+
+
+            return View();
+        }
     }
 }
